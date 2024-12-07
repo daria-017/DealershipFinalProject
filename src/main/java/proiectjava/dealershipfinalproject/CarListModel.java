ec206960f1d01ae0f0cd6bc11959662f625b869c
Alexandru-Arhip <arhp.alexandru@gmail.com>
2024-12-07 15:03:19 +0200

    Added class for AccountDetails list!

---
+++ src/main/java/proiectjava/dealershipfinalproject/CarListModel.java
@@ -0,0 +1,28 @@
+package proiectjava.dealershipfinalproject;
+
+import javax.swing.AbstractListModel;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CarListModel extends AbstractListModel<String> {
+    private final List<String> cars = new ArrayList<>();
+
+    @Override
+    public int getSize() {
+        return cars.size();
+    }
+
+    @Override
+    public String getElementAt(int index) {
+        return cars.get(index);
+    }
+
+    public void addCar(String car) {
+        cars.add(car);
+        int index = cars.size() - 1;
+        fireIntervalAdded(this, index, index);
+    }
+}
